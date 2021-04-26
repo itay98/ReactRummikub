6321b73b4cc23f921282ea6509b8749c56d174ee
Itay Eshet <itay1409@gmail.com>
2021-04-26 18:22:54 +0300

    ssl

--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
+  "production": {
+      "dialect":"postgres",
+      "ssl": true,
+      "dialectOptions": {
+          "ssl": true
+      }
+  }
+});
 sequelize.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
   .catch(err => console.error('Unable to connect to the database:', err));
