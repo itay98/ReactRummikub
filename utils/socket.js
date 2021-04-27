4a4166c409171b5cc67484ec55f36008ad438f18
Itay Eshet <itay1409@gmail.com>
2021-04-27 18:40:12 +0300

    cors

--- utils/socket.js
+++ utils/socket.js
@@ -4,8 +4,8 @@ const { drawTile, setRackWithTiles, checkBoard } = require('./game');
 const { updateBalance } = require('../controllers/users');
 const pointRate = { 3: 1, 10: 0.9, 32: 0.875, 100: 0.85 };
 function createSocket(app) {
-    const server = require("http").createServer(app,{cors:{ origin: '*', methods: ['GET', 'POST'] }});
-    const io = require("socket.io")(server);
+    const server = require("http").createServer(app);
+    const io = require("socket.io")(server,{cors:{ origin: '*', methods: ['GET', 'POST'] }});
     io.on("connection", socket => {
         console.log("New client connected");
         let room, player, plys, rId, num;
